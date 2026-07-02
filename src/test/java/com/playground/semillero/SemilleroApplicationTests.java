ff86e15116abd8dfaca4937a34484ac00a6567eb
joan566 <joansebastianvergaravalencia@gmail.com>
2026-07-01 20:31:11 -0500

    First commit

---
+++ src/test/java/com/playground/semillero/SemilleroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.playground.semillero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SemilleroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
